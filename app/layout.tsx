3121cc9b41461f4c68310c33bb87803f75f12e2d
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2023-11-19 14:45:20 -0500

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Providers } from "../context/providers";
-import Header from '@/components/layout/Header';
-import Footer from '@/components/layout/Footer';
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ULC | Dawnstar",
-  description: "ULC is an all-in-one lighting controller for Non-ELS vehicles in FiveM! It uses the extra-based lighting stages on your vehicles and adds extra automation and improvements to create amazing, realistic, and fully-configurable lighting controls.",
+  description:
+    "ULC is an all-in-one lighting controller for Non-ELS vehicles in FiveM! It uses the extra-based lighting stages on your vehicles and adds extra automation and improvements to create amazing, realistic, and fully-configurable lighting controls.",
 };
 
 export default function RootLayout({
@@ -22,9 +24,10 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Providers>
-          <Header/>
+          <Header />
           {children}
-          <Footer/>
+          <Footer />
+          <Analytics />
         </Providers>
       </body>
     </html>
