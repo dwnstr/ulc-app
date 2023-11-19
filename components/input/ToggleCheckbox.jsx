e72933a30893b05f6558aef83663b2cd64360647
Flohhhhh <camgus322@gmail.com>
2023-11-19 09:25:45 +0000

    Fix checkbox color

--- components/input/ToggleCheckbox.jsx
+++ components/input/ToggleCheckbox.jsx
@@ -18,7 +18,7 @@ const ToggleCheckbox = (props) => {
         key={`${configId}${field}`}
         onCheckedChange={handleChange}
         checked={value}
-        className='hover:brightness-125 flex h-5 w-5 items-center justify-center rounded-[4px] bg-shark-600 border border-shark-500aria-checked:bg-emerald-600 outline-none focus:outline-emerald-500'
+        className='hover:brightness-125 flex h-5 w-5 items-center justify-center rounded-[4px] bg-shark-600 border border-shark-500 aria-checked:bg-emerald-600 outline-none focus:outline-emerald-500'
       >
         <Checkbox.Indicator className=''>
           <Check className='w-4 h-4' />
