84425e0f1aa693b7bca18b335d6c50befb82cced
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2023-11-19 17:05:33 -0500

    Attempt to fix opengraph-image

--- app/generator/layout.jsx
+++ app/generator/layout.jsx
@@ -1,10 +1,10 @@
-
 export const metadata = {
-    title: "ULC Generator",
-    description: "A tool to create ULC configurations using a GUI. ULC is an all-in-one lighting controller for GTA and FiveM vehicles.",
+  title: "ULC Generator",
+  description:
+    "A tool to create ULC configurations using a GUI. ULC is an all-in-one lighting controller for GTA and FiveM vehicles.",
+  metadataBase: new URL("https://ulc.dwnstr.com/generator/"),
 };
-  
 
-export default function GeneratorLayout ({children}) {
-    return (<>{children}</>)
+export default function GeneratorLayout({ children }) {
+  return <>{children}</>;
 }
