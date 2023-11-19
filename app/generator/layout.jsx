453ef084888580019201037e9ac2ff7e90eefc5c
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2023-11-19 17:08:59 -0500

    Update layout.jsx

--- app/generator/layout.jsx
+++ app/generator/layout.jsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: "ULC Generator",
   description:
     "A tool to create ULC configurations using a GUI. ULC is an all-in-one lighting controller for GTA and FiveM vehicles.",
-  metadataBase: new URL("https://ulc.dwnstr.com/generator/"),
+  metadataBase: new URL("https://ulc.dwnstr.com/"),
 };
 
 export default function GeneratorLayout({ children }) {
